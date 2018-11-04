968a85e4fa3d65dc137eaaf3113d544173d456ec
nagisa <3094589063@qq.com>
2018-11-04 16:01:33 +0800

    finished

--- http.go
+++ http.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+	})
+	http.ListenAndServe(":9090", nil)
 }
